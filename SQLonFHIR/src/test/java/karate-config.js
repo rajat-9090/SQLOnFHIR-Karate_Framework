ac65574b95f7b517d04753935b83a49ef3dff421
rajat <rajat@gamil.com>
2025-06-28 12:04:24 +0530

    init commit karate sqlonfhir

--- SQLonFHIR/src/test/java/karate-config.js
+++ SQLonFHIR/src/test/java/karate-config.js
@@ -6,8 +6,7 @@ function fn() {
   }
   var config = {
     env: env,
-    ApiKeyValue: 'ApiKey AvG1cZHyFjLB3RqiDKproQMp9yxwtFiwqiAdreKHecQyYKhwxJpA2Xtklp1l72gdT95rr3U6Pg8WcDokHk3wimbsXRKa8Lowu0Eh5b8ZpXv584h2ku6CJsbD7NOmPj0c',
-    host: 'https://sqlonfhir-sqlonfhir.fhir.azurehealthcareapis.com',
+
 
   }
   if (env == 'dev') {
